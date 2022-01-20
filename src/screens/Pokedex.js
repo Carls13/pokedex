ee63bad78b676f7bf72e4ab28f6a89c603fe3931
Carlos Hernández <carlos.hernandez@validadtedid.com>
2022-01-19 21:17:34 -0400

    Setting project up

---
+++ src/screens/Pokedex.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const PokedexScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+}
+
+export default PokedexScreen;
